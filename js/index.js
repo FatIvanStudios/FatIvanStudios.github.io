695567d989c2d5a5e86d350ebf16a85920c3dcf5
NotMie-cmd <fativanstudios@gmail.com>
2024-11-24 18:03:34 +0100

    f

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+
     const viewersElement = document.getElementById("github-website-viewers");
 
     const githubApiUrl = 'https://api.github.com/repos/fativanstudios/fativanstudios.github.io';
@@ -11,7 +11,13 @@ document.addEventListener("DOMContentLoaded", function() {
         })
         .catch(error => {
             console.error('Error fetching GitHub viewers:', error);
-            viewersElement.innerText = 'Error fetching viewer count';
+            viewersElement.innerText = 'Error fetching viewer countxcvbcvbcvbfdxhfcghgccvhxcvhxvch';
         });
-});
+
+    const btn = document.getElementById('ok-btn');
+    function change() {
+        btn.innerText = "sorry but i'm too lazy to make the button working"
+        btn.style.height = '190px'
+        btn.style.width = '90px'
+    }
 // why do you work on live preview but not on github
