b6905e2dc626c5051a3cef946a46bd8162f43153
NotMie-cmd <fativanstudios@gmail.com>
2024-11-25 07:38:27 +0100

    change a LOT of stuff
    aaaaaaaaaaaaaaaaaaaaaa

--- js/index.js
+++ js/index.js
@@ -1,23 +1,8 @@
-
-    const viewersElement = document.getElementById("github-website-viewers");
-
-    const githubApiUrl = 'https://api.github.com/repos/fativanstudios/fativanstudios.github.io';
-
-    fetch(githubApiUrl)
-        .then(response => response.json())
-        .then(data => {
-            const viewerCount = data.watchers_count;
-            viewersElement.innerText = `Viewers: ${viewerCount}`;
-        })
-        .catch(error => {
-            console.error('Error fetching GitHub viewers:', error);
-            viewersElement.innerText = 'Error fetching viewer countxcvbcvbcvbfdxhfcghgccvhxcvhxvch';
-        });
-
-    const btn = document.getElementById('ok-btn');
-    function change() {
-        btn.innerText = "sorry but i'm too lazy to make the button working"
-        btn.style.height = '190px'
-        btn.style.width = '90px'
-    }
-// why do you work on live preview but not on github
+var donateRBX = document.getElementById('donate-RBX');
+function openRobloxGamepasses() {
+    window.open('https://www.roblox.com/games/81295772710470/Miejsce-tw-rcy-NotTheMisko#!/store');
+}
+var email = document.getElementById('e');
+function makeTheEmailVisibleIg() {
+    email.style.display = 'block';
+}
